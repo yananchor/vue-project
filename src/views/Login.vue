5ed63d483c59b2153b4334093e6519db2d015945
feng <bluesky.yan@outlook.com>
2022-05-26 23:16:25 +0800

    add element-plus and update login page content

---
+++ src/views/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <header></header>
+  <h1>Element Plus</h1>
+  <p>Element Plus and Vue3</p>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Login",
+});
+</script>
