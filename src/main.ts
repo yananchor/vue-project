32cdc9862ba40c60c7d44685d05fb102be617886
EmilyFeng <yanyanfeng@wistronits.com>
2022-05-27 17:51:06 +0800

    update login page and add element plus icon

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+const app = createApp(App);
+app.use(ElementPlus).use(store).use(router).mount("#app");
+// Register all icon
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,54 @@
 <template>
-  <header></header>
-  <h1>Element Plus</h1>
-  <p>Element Plus and Vue3</p>
+  <div class="common-layout">
+    <el-container class="container">
+      <el-header>
+      </el-header>
+      <el-main>
+        <h1>Element Plus</h1>
+        <p>Element Plus and Vue3</p>
+        <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+          <el-tab-pane label="账号密码登陆" name="userName">
+            <el-form>
+              <el-input v-model="formData.userName" />
+              <el-input v-model="formData.password" />
+            </el-form>
+          </el-tab-pane>
+          <el-tab-pane label="手机号登陆" name="phoneNumber">phoneNumber</el-tab-pane>
+        </el-tabs>
+      </el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
-
 export default defineComponent({
   name: "Login",
+  data() {
+    return {
+      activeName: 'userName',
+      formData: {
+        userName: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    handleClick() {
+
+    }
+  }
 });
 </script>
+<style>
+.container {
+  width: 400px;
+  margin: 0 auto;
+}
+.demo-tabs>.el-tabs__content {
+  padding: 32px;
+  color: #6b778c;
+  font-size: 32px;
+  font-weight: 600;
+}
+</style>
