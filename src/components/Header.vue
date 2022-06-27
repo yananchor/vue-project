4871c5cc4ad08ade5a1977a3514c14795b35e3e1
EmilyFeng <yanyanfeng@wistronits.com>
2022-06-27 17:14:06 +0800

    add home page layout and header styles

---
+++ src/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <img src="../assets/logo.png" alt="" width="30" height="30">
+    <span>Element Plus</span>
+  </div>
+  <div>
+    <el-icon>
+      <Search />
+    </el-icon>
+    <el-icon>
+      <Bell />
+    </el-icon>
+    <el-dropdown>
+      <div class="el-dropdown-link">
+        <el-avatar src="../assets/initAvatar.png" size="small"></el-avatar>
+        <span>{{ userName }}</span>
+      </div>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item icon="user">个人中心</el-dropdown-item>
+          <el-dropdown-item icon="setting">个人设置</el-dropdown-item>
+          <el-dropdown-item icon="switchButton" divided>退出登陆</el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
+    <el-dropdown>
+      <div class="el-dropdown-link">
+        <el-icon>
+          <Film />
+        </el-icon>
+      </div>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item>简体中文</el-dropdown-item>
+          <el-dropdown-item>English</el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Header',
+  data() {
+    return {
+      userName: 'Emily'
+    }
+  }
+});
+</script>
+
+<style scoped>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,65 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <el-container>
+      <el-header>
+        <Header />
+      </el-header>
+      <el-container class="main-container">
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Header from '@/components/Header.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    HelloWorld,
-  },
+    Header,
+  }
 });
 </script>
+
+<style lang="scss">
+.home {
+  width: 100%;
+  height: 100vh;
+}
+
+.main-container {
+  height: calc(100vh - 50px);
+}
+
+.el-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 50px;
+  background-color: #001529;
+  color: #FFF;
+  cursor: pointer;
+
+  div {
+    width: auto;
+    height: 100%;
+    line-height: 50px;
+    display: flex;
+    align-items: center;
+
+    img,
+    i,
+    span {
+      color: #FFF;
+      margin-right: 15px;
+    }
+  }
+}
+
+.el-main {
+  background-color: #f0f2f5;
+}
+</style>
